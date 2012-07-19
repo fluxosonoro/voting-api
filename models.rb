d230b552aff2cdd24c599cf4e7bc7f3102e4636c
APIs <registros.tech@ciudadanointeligente.cl>
2012-07-19 16:50:37 -0400

    deleted 'order' field

--- models.rb
+++ models.rb
@@ -12,23 +12,23 @@ class Bill
 
   # Relations
   embeds_many :events, :autosave => true
-  belongs_to :table
-  
+#  add reference to tables (probably a table array)
+#  belongs_to :table  
 
   # Fields
-  field :id, :class => String ,:meta => ['display_name'=>'Boletin', 'link_to_detail'=>true, 'type'=>'text', 'order'=>0, 'should_be_shown_in_list'=>true]
-  field :title, :class => String ,:meta => ['type'=>'false','display_name'=>'Titulo', 'order'=>1, 'should_be_shown_in_list'=>true]
-  field :summary, :class => String ,:meta => ['type'=>'false','display_name'=>'Resumen', 'order'=>2, 'should_be_shown_in_list'=>false]
-  field :tags, :class => Array ,:meta => ['type'=>'tag','display_name'=>'Tags', 'order'=>3, 'should_be_shown_in_list'=>false]
-  field :matters, :class => Array ,:meta => ['type'=>'tag','display_name'=>'Materias', 'order'=>4, 'should_be_shown_in_list'=>false]
-  field :stage, :class => String ,:meta => ['type'=>'text','display_name'=>'Etapa', 'order'=>5, 'should_be_shown_in_list'=>true]            # Current Stage
-  field :creation_date, :type => DateTime ,:meta => ['type'=>'date','display_name'=>'Fecha de Creación', 'order'=>6, 'should_be_shown_in_list'=>true]
-  field :publish_date, :type => DateTime ,:meta => ['type'=>'date','display_name'=>'Fecha Publicación', 'order'=>7, 'should_be_shown_in_list'=>true]
-  field :authors, :class => Array ,:meta => ['type'=>'text','display_name'=>'Autores', 'order'=>8, 'should_be_shown_in_list'=>false]
-  field :origin_chamber, :class => String ,:meta => ['type'=>'text','display_name'=>'Cámara de origen', 'order'=>9, 'should_be_shown_in_list'=>false]
-  field :current_urgency, :class => String ,:meta => ['type'=>'text','display_name'=>'Urgencia', 'order'=>10, 'should_be_shown_in_list'=>false]
-  field :table_history, :class => Array ,:meta => ['order'=>11, 'should_be_shown_in_list'=>false]
-  field :link_law, :class => Array ,:meta => ['type'=>'text','display_name'=>'Link a la ley', 'order'=>12, 'should_be_shown_in_list'=>false]
+  field :id, :class => String ,:meta => ['display_name'=>'Boletin', 'link_to_detail'=>true, 'type'=>'text', 'should_be_shown_in_list'=>true]
+  field :title, :class => String ,:meta => ['type'=>'false','display_name'=>'Titulo', 'should_be_shown_in_list'=>true]
+  field :summary, :class => String ,:meta => ['type'=>'false','display_name'=>'Resumen', 'should_be_shown_in_list'=>false]
+  field :tags, :class => Array ,:meta => ['type'=>'tag','display_name'=>'Tags', 'should_be_shown_in_list'=>false]
+  field :matters, :class => Array ,:meta => ['type'=>'tag','display_name'=>'Materias', 'should_be_shown_in_list'=>false]
+  field :stage, :class => String ,:meta => ['type'=>'text','display_name'=>'Etapa', 'should_be_shown_in_list'=>true]            # Current Stage
+  field :creation_date, :type => DateTime ,:meta => ['type'=>'date','display_name'=>'Fecha de Creación', 'should_be_shown_in_list'=>true]
+  field :publish_date, :type => DateTime ,:meta => ['type'=>'date','display_name'=>'Fecha Publicación', 'should_be_shown_in_list'=>true]
+  field :authors, :class => Array ,:meta => ['type'=>'text','display_name'=>'Autores', 'should_be_shown_in_list'=>false]
+  field :origin_chamber, :class => String ,:meta => ['type'=>'text','display_name'=>'Cámara de origen', 'should_be_shown_in_list'=>false]
+  field :current_urgency, :class => String ,:meta => ['type'=>'text','display_name'=>'Urgencia', 'should_be_shown_in_list'=>false]
+  field :table_history, :class => Array ,:meta => ['should_be_shown_in_list'=>false]
+  field :link_law, :class => Array ,:meta => ['type'=>'text','display_name'=>'Link a la ley', 'should_be_shown_in_list'=>false]
 
   # Indexes
   index :id, :unique => true
@@ -42,12 +42,16 @@ class Bill
   end
   
   include Sunspot::Mongoid
-  searchable :auto_remove => true do
-    text :id#, :stored => true
+#  searchable :auto_remove => true do
+  searchable do
+    string :id#, :stored => true
     text :title#, :stored => true
-#    text :summary
+    text :summary
     text :stage
+    time :creation_date
+    time :publish_date
     text :origin_chamber
+    text :current_urgency
   end
 end
 
